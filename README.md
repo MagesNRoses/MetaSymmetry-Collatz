def308ff01c163d25862441ea3ae32de9df66ad4
MagesNRoses <santiagovillalbaehv@gmail.com>
2025-06-24 19:08:48 +0000

    Primer análisis de regiones ABCD

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MetaSymmetry-Collatz
-Undergraduate research on stratified symmetries in Collatz dynamics
+Investigación estudiantil sobre simetrías estratificadas en la dinámica de Collatz
+
+## Descubrimiento Clave
+He observado que las trayectorias de Collatz se organizan en 4 regiones dinámicas:
+- **Región A**: x > ε > log₂3
+- **Región B**: ε > x > log₂3
+- **Región C**: log₂3 > x > ε
+- **Región D**: log₂3 > ε > x
+
+**Hallazgo sorprendente**: Las trayectorias que pasan por la región ABC convergen más rápido.
+
+## Primeros Resultados
+![Regiones Collatz para n=27](collatz_regions.png)
+
+## Cómo replicar
+```bash
+git clone https://github.com/MagesNRoses/MetaSymmetry-Collatz.git
+pip install -r requirements.txt
+jupyter notebook notebooks/Basic_Region_Analysis.ipynb
---
+++ src/collatz-functions.py
@@ -0,0 +1,52 @@
+def collatz_sequence(n):
+    """Genera secuencia de Collatz para un número n"""
+    sequence = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        sequence.append(n)
+    return sequence
+
+def classify_region(x, epsilon=0.1, log23=1.58496):
+    """Clasifica el punto en las regiones A,B,C,D"""
+    if x > epsilon > log23:
+        return 'A'
+    elif epsilon > x > log23:
+        return 'B'
+    elif log23 > x > epsilon:
+        return 'C'
+    elif log23 > epsilon > x:
+        return 'D'
+    return 'U'  # No clasificado
+
+def analyze_trajectory(n):
+    """Analiza una trayectoria completa"""
+    seq = collatz_sequence(n)
+    regions = []
+    j, k = 0, 0  # Contadores de operaciones
+    
+    for i in range(1, len(seq)):
+        # Determinar tipo de operación
+        if seq[i] == 3 * seq[i-1] + 1:
+            j += 1
+            op = '3n+1'
+        else:
+            k += 1
+            op = 'n/2'
+        
+        # Calcular razón x
+        total_ops = j + k
+        x = j / total_ops if total_ops > 0 else 0
+        
+        # Clasificar región
+        region = classify_region(x)
+        regions.append((seq[i-1], op, x, region))
+    
+    return {
+        'n': n,
+        'steps': len(seq) - 1,
+        'sequence': seq,
+        'regions': regions
+    }
